0389703f662950f3b9782f3f4f20d93b8391757f
achilleas01 <achilleas01@yahoo.gr>
2022-09-17 10:02:00 +0300

    2 files modified

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from 'src/app/models/models';
-import { UserService } from 'src/app/services/user.service';
+import { User } from './src/app/models/models';
+// import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  user$!:Observable<User>;
-  constructor(private userService:UserService) { }
+  // user$!: Observable<User>;
+  constructor() {}
 
   ngOnInit(): void {
-   this.user$ = this.userService.user$;
+    // this.user$ = this.userService.user$;
   }
-
 }
