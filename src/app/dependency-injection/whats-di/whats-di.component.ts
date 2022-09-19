8c7051ae05e8e49b2a025a112c581751c543d8b7
achilleas01 <achilleas01@yahoo.gr>
2022-09-19 12:39:20 +0300

    1 file modified

--- src/app/dependency-injection/whats-di/whats-di.component.ts
+++ src/app/dependency-injection/whats-di/whats-di.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { map } from 'rxjs/operators';
-import { Rights } from 'src/app/models/models';
-import { DataService } from 'src/app/services/data.service';
+import { Rights } from '../../models/models';
+import { DataService } from '../../services/data.service';
 import { ConsoleLoggerService } from './console-logger.service';
 import { CountService, RightsService } from '../../services/rights.service';
 import { EventLoggerService } from './event-logger.service';
@@ -13,12 +13,9 @@ import { EventLoggerService } from './event-logger.service';
   styleUrls: ['./whats-di.component.scss'],
 })
 export class WhatsDiComponent implements OnInit {
-  constructor(private loggerService:ConsoleLoggerService) {
-  }
+  constructor(private loggerService: ConsoleLoggerService) {}
 
   ngOnInit(): void {
-    this.loggerService
-      .log('data')
-      .subscribe();
+    this.loggerService.log('data').subscribe();
   }
 }
